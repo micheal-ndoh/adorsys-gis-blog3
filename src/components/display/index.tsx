91ecd858ca72bce0128953e7232fafb797965542
micheal-ndoh <michaelndoh9@gmail.com>
2025-08-19 16:47:56 +0100

    chore: update yarn.lock and refactor blog page component; clean up imports and improve error handling

--- src/components/display/index.tsx
+++ src/components/display/index.tsx
@@ -1,13 +1,9 @@
-'use client';
+"use client";
 
 import dynamic2 from "next/dynamic";
-import { Skeleton } from "@blog/components/loading";
 
-const Display = dynamic2(() => import('./display'), {
-    ssr: false,
-    loading: () => <Skeleton className="h-64 w-full mb-8" />,
+const Display = dynamic2(() => import("./display"), {
+  ssr: false,
 });
 
 export default Display;
-
-
