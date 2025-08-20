1ebdec66e300d4e12f9cff06ca60abc4c9eee5b5
lele-maxwell <leleivanlele22@gmail.com>
2025-08-20 05:55:52 +0100

    style(MainLayout): update navbar layout and spacing for improved responsiveness and visual consistency

--- src/app/(read)/layout.tsx
+++ src/app/(read)/layout.tsx
@@ -2,7 +2,6 @@ import {Container} from "@blog/components/container";
 import Link from "next/link";
 import Image from "next/image";
 import icon from "@blog/components/icon.svg";
-import {ThemeToggle} from "@blog/components/theme";
 import type {PropsWithChildren} from "react";
 
 export default function MainLayout({
@@ -12,16 +11,15 @@ export default function MainLayout({
         <>
             <div className='sticky top-0 z-40 bg-base-300'>
                 <Container className='py-0'>
-                    <nav className='navbar'>
-                        <div className='navbar-start flex gap-4'>
-                            <Link href='/' className='flex flex-row items-center gap-2'>
-                                <Image src={icon} className='w-8' alt='logo'/>
-                                <span className='text-xl font-extrabold uppercase' color='ghost'>Learn</span>
+                    <nav className='navbar min-h-16'>
+                        <div className='navbar-start flex gap-2 sm:gap-4'>
+                            <Link href='/' className='flex flex-row items-center gap-1.5 sm:gap-2'>
+                                <Image src={icon} className='w-6 sm:w-8' alt='logo'/>
+                                <span className='text-lg sm:text-xl font-extrabold uppercase' color='ghost'>Learn</span>
                             </Link>
                         </div>
 
-                        <div className='navbar-end flex gap-4'>
-                            <ThemeToggle/>
+                        <div className='navbar-end flex gap-2 sm:gap-4'>
                         </div>
                     </nav>
                 </Container>
